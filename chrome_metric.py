c1dbda62d9ec72733b7a3bbad816d3d0e4e50d1c
Alex Yancey <alexyancey3@gmail.com>
2014-06-17 00:34:49 -0700

    Partial implementation of Chrome metric

--- chrome_metric.py
+++ chrome_metric.py
@@ -5,17 +5,35 @@ import json
 from pprint import pprint
 import sys
 import os
+from colorama import init, Fore, Back, Style
+init()
+
+def scanextension(path):
+	if len(os.listdir(path)) >= 1:
+		if (os.path.isfile(path + '/' + os.listdir(path)[0] + '/manifest.json')):
+			print 'Found manifest at: ' + Fore.YELLOW + path + '/' + os.listdir(path)[0] + '/manifest.json' + Fore.RESET
+			data = json.loads(open(path + '/' + os.listdir(path)[0] + '/manifest.json', 'rb').read())
+			print 'Extension name: ' + data["name"]
+			print 'Extension version: ' + data["version"]
+			#print str(data["content_scripts"][0]["all_frames"]) + ' suspicious'
+			print 'Permissions required:'
+			for permission in data["permissions"]:
+				if permission == 'management':
+					print Fore.RED + permission + Fore.RESET + ' - ' + Fore.YELLOW + ' This extension is allowed to manage other extensions.' + Fore.RESET
+				elif permission == 'http://*/*':
+					print Fore.RED + permission + Fore.RESET + ' - ' + Fore.YELLOW + ' This extension is allowed to match with ALL non-secure webpages.' + Fore.RESET
+				else:
+					print permission
+
+				
+
 
 def execute():
 	sys.stdout.write('Is Google Chrome installed? ')
 	if os.path.exists(os.environ['LOCALAPPDATA'] + '/Google/Chrome/User Data/Default/Preferences'):
 		print 'Yes'
-	#data = json.loads(open('manifest.json', 'rb').read())
-	#print data["name"]
-	#print data["version"]
-	#print str(data["content_scripts"][0]["all_frames"]) + ' suspicious'
-	#for permissions in data["permissions"]:
-	#print permissions
+	scanextension('hiollpollmacojhigkifgnajhgnoiool')
+
 
 if __name__ == "__main__":
 	execute()
