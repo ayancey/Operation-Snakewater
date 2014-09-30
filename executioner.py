d00634a5061af152ca102129d24ce37f556d1cd5
Alex Yancey <alexyancey3@gmail.com>
2014-09-30 08:43:17 -0700

    Added pup.py

--- executioner.py
+++ executioner.py
@@ -1,11 +1,15 @@
 # Snakewater Executioner
 # Copyright Meanberg Design 2014
 # Version 1.0
+# Developed in part Ettlin AP CS Period 1
+
 import sys
 import netstat_metric
 import registry_metric
 import chrome_metric
 import startup_metric
+import pup
+
 
 print 'Snakewater Executioner v1.0 by Meanberg Design'
 sys.stdout.write("Press any key to begin...")
